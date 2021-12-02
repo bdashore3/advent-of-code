10c52271e29079e035f385added70eb2db950f00
kingbri <bdashore3@gmail.com>
2021-12-02 14:10:50 -0500

    Optimize day 1 solution
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- day1/part1/solution.js
+++ day1/part1/solution.js
@@ -1,26 +1,26 @@
 const fs = require('fs')
 
-const rawData = fs.readFileSync('../input.json', {encoding: 'utf-8'})
-const inputJson = JSON.parse(rawData)
+const rawData = fs.readFileSync('../input.txt', {encoding: 'utf-8'})
+
+const inputList = rawData.split('\n').map((x) => {
+    return Number(x)
+})
 
 // Start at the first element of the list so a false positive isn't fired
-let previousElement = inputJson.list[0]
+let previousElement = inputList[0]
 
 // Counter for the elements greater than the previous one
 let amountGreater = 0
 
 // Checks if the previous element is greater than the current one
-for (element of inputJson.list) {
+for (element of inputList) {
     if (element > previousElement) {
-        //console.log(`${element} is greater than ${previousElement}`)
-
         amountGreater++
-    } else {
-        //console.log(`${element} is less than or equal to ${previousElement}`)
     }
+
     previousElement = element
 }
 
 // Prints out the array length and part 1 answer
-console.log(`Array length ${inputJson.list.length}`)
+console.log(`Array length ${inputList.length}`)
 console.log(`The increasing digit total is: ${amountGreater}`)
--- day1/part2/solution.js
+++ day1/part2/solution.js
@@ -1,25 +1,22 @@
 const fs = require('fs')
 
-const rawData = fs.readFileSync('../inputtest.json', {encoding: 'utf-8'})
-const inputJson = JSON.parse(rawData)
+const rawData = fs.readFileSync('../input.txt', {encoding: 'utf-8'})
+
+const inputList = rawData.split('\n').map((x) => {
+    return Number(x)
+})
 
 // Start at the first element of the list so a false positive isn't fired
-let previousSum = inputJson.list[0] + inputJson.list[1] + inputJson.list[2]
+let previousSum = inputList[0] + inputList[1] + inputList[2]
 
 // Counter for the sums greater than the previous one
 let amountGreater = 0
 
 // Checks if the previous sum is greater than the current one
-for (let index = 0; index < inputJson.list.length; index++) {
-    //console.log(`Index: ${index}, typeof index: ${typeof(index)} output ${inputJson.list[index]}`)
-
-    let sum = inputJson.list[index] + inputJson.list[index + 1] + inputJson.list[index + 2]
+for (let index = 0; index < inputList.length; index++) {
+    let sum = inputList[index] + inputList[index + 1] + inputList[index + 2]
     if (sum > previousSum) {
-        //console.log(`${sum} is greater than ${previousSum}`)
-
         amountGreater++
-    } else {
-        //console.log(`${sum} is less than ${previousSum}`)
     }
 
     // Set the previous sum to the current one
@@ -27,5 +24,5 @@ for (let index = 0; index < inputJson.list.length; index++) {
 }
 
 // Prints out the array length and part 1 answer
-console.log(`Array length ${inputJson.list.length}`)
+console.log(`Array length ${inputList.length}`)
 console.log(`The greater sum total is: ${amountGreater}`)
